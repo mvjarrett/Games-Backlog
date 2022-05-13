1ab38e06886a645f27823f86726a8b460e982c88
Matt <Mvjarrett92@gmail.com>
2022-05-13 12:57:22 -0400

    working on infinite scroll

--- client/src/app/components/igdb-results/igdb-results.component.ts
+++ client/src/app/components/igdb-results/igdb-results.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IgdbResultsService } from '../../services/igdbResults.service';
 import { igGame } from '../../models/igGame';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-igdb-results',
@@ -8,24 +9,34 @@ import { igGame } from '../../models/igGame';
   styleUrls: ['./igdb-results.component.css'],
 })
 export class IgdbResultsComponent implements OnInit {
+  infiniteScroll: InfiniteScrollDirective;
   constructor(private igdbResults: IgdbResultsService) {}
   igGames: igGame[] = [];
-  throttle = 0;
-  distance = 2;
-  page = 1;
+  throttle = 1;
+  distance = 1;
+  @ViewChild(InfiniteScrollDirective)
+  set appScroll(directive: InfiniteScrollDirective) {
+    this.infiniteScroll = directive;
+  }
   ngOnInit(): void {
-    this.igdbResults.topGames().subscribe((data) => {
+    this.igdbResults.infiniteGames().subscribe((data) => {
       if (data) {
         this.igGames = data;
       }
     });
   }
-  // onScroll(): void {
-  //   this.igdbResults.infiniteGames().subscribe((data) => {
-  //     if (data) {
-  //       this.igGames = data;
-  //       this.igGames.push(...this.igGames)
-  //     }
-  //   });
-  // }
+  onScroll(): void {
+    this.igdbResults.infiniteGames().subscribe((data) => {
+      if (data) {
+        this.igGames = data;
+        this.igGames.push(...this.igGames);
+        this.infiniteScroll.ngOnDestroy();
+
+      }        this.infiniteScroll.setup();
+    });
+  }
 }
+
+// this.igdbResults.topGames().subscribe((data) => {
+//   if (data) {
+//     this.igGames = data;
