da9a0a0a5da25d501f47c466993f9703893b9449
Matt <mvjarrett92@gmail.com>
2022-05-29 14:46:14 -0700

    add conditional for no backlog when logged in

--- client/src/app/components/games-list/games-list.component.ts
+++ client/src/app/components/games-list/games-list.component.ts
@@ -11,16 +11,20 @@ import { gameObject } from 'src/app/models/gameobject';
 })
 export class GamesListComponent implements OnInit {
   logItems: igGame[] = [];
-
+backlogItems = false;
   constructor(private gameService: GameService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    console.log('prebacklog = ', this.backlogItems)
     this.gameService.GetGames().subscribe((data) => {
-      if (data) {
+      if (data) { 
+        console.log('backlog = ', this.backlogItems)
+
         const backlogIds = data.map((x) => x.id);
         this.gameService.backlogInfo(backlogIds).subscribe((info) => {
           if (info) {
             this.logItems = info;
+            this.backlogItems = true;
           }
         });
       }
