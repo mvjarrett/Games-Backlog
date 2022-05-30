363ee602b2a731b17dff151fe2adb7ee1034d14d
Matt <Mvjarrett92@gmail.com>
2022-05-30 13:53:22 -0400

    restructure

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -12,6 +12,13 @@ var corsOptions = {
   origin: "*",
   credentials: true
 };
+// function requireHTTPS(req, res, next) {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//       return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// }
 
 //middleware
 app.use(cookieParser())
