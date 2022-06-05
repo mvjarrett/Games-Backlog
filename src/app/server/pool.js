64e9162c6078573af1bb1ee7d91b7ceefe8276a9
Matt <Mvjarrett92@gmail.com>
2022-06-05 02:45:03 -0400

    fix server.js

--- src/app/server/pool.js
+++ src/app/server/pool.js
@@ -1,20 +1,20 @@
 const dotenv = require ('dotenv')
 const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-
-
-const pool = new Pool({    
-    user: process.env.USER_KEY,
-    password: process.env.PASS_KEY1,
-    host: process.env.HOST_KEY,
-    port: process.env.PORT_KEY,
-    database: process.env.DB_KEY
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+
+
+// const pool = new Pool({    
+//     user: process.env.USER_KEY,
+//     password: process.env.PASS_KEY1,
+//     host: process.env.HOST_KEY,
+//     port: process.env.PORT_KEY,
+//     database: process.env.DB_KEY
+// });
 
 module.exports = pool;
