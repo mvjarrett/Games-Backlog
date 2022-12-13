76567ef6ba40f56c9b134d14b38b00672c9130eb
Matt <Mvjarrett92@gmail.com>
2022-12-13 17:56:40 -0500

    add platform/genre tabs in nav, fix function

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,6 +8,10 @@ import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { SocialAuthService } from '@abacritt/angularx-social-login';
+import genre from 'src/app/models/genre';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import platform from 'src/app/models/platform';
 
 @Component({
   selector: 'app-navbar',
@@ -16,14 +20,19 @@ import { SocialAuthService } from '@abacritt/angularx-social-login';
 })
 export class NavbarComponent implements OnInit {
   searchTerm: string;
+  genres: genre[];
+  platforms: platform[];
   searchForm = new UntypedFormGroup({
     search: new UntypedFormControl('', Validators.required),
   });
+  glist: any[]
+  genreUrl = "externalgames/genres"
+  platformUrl = "externalgames/platforms"
   headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers':
-      'Origin, X-Requested-With, Content-Type, Accept',
+      'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token',
   };
   constructor(
     private http: HttpClient,
@@ -31,7 +40,12 @@ export class NavbarComponent implements OnInit {
     private authService: SocialAuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+this.getGenres().subscribe((res: genre[]) => this.genres = res)
+this.getPlatforms().subscribe((res: platform[]) => this.platforms = res)
+
+
+  }
   onSubmit(): void {
     this.searchTerm = this.searchForm.controls['search'].value;
 
@@ -44,6 +58,24 @@ export class NavbarComponent implements OnInit {
       });
   }
 
+
+  getGenres(): Observable<genre[]>{
+    let genreBody = 'fields name; limit 200;'
+  return this.http.post<genre[]>(this.genreUrl, genreBody, {
+    headers: this.headers
+     
+    }).pipe(map((response:genre[]) => response as genre[]));
+  }
+
+  getPlatforms(): Observable<platform[]>{
+    let platformBody = 'fields name; limit 200; sort id :asc;'
+  return this.http.post<platform[]>(this.platformUrl, platformBody, {
+    headers: this.headers
+     
+    }).pipe(map((response:genre[]) => response as genre[]));
+  }
+
+
   logout() {
     localStorage.clear();
     this.authService.signOut();
@@ -52,3 +84,5 @@ export class NavbarComponent implements OnInit {
 
   
 }
+
+
