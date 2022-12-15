8020b19d0e2d011bac799782b1e9815cac055c30
Matt <Mvjarrett92@gmail.com>
2022-12-15 12:29:27 -0500

    refactor infinite scroll, igdbResults service

--- src/app/services/igdbResults.service.ts
+++ src/app/services/igdbResults.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { igGame } from '../models/igGame';
+
+
+// =========== Below is a list of strings I moved out of the functions below to make them more human-readable
+
+const searchStrings = {
+  topBody:
+    'fields name, cover.url; limit 50; where rating >80 & aggregated_rating != null & rating != null; sort total_rating desc; ',
+  platformString1:
+    'fields name, cover.url; limit 50; where category = 0 & release_dates.platform = ',
+  platformString2: ' & rating != null; sort total_rating desc;',
+  genreString1:
+    'fields name, cover.url; limit 50; where category = 0 & genres = ',
+  genreString2: ' & rating != null; sort total_rating desc;',
+  searchString: 'fields name, cover.url; limit 200; search ',
+};
+
 
 
 @Injectable({
@@ -15,21 +31,18 @@ export class IgdbResultsService {
       'Origin, X-Requested-With, Content-Type, Accept',
   };
   offset = 50;
-  topBody =
-    'fields name, cover.url; limit 50; where rating >85 & aggregated_rating != null & rating != null; sort total_rating desc; ';
+  body: string;
 
   constructor(private http: HttpClient) {}
 
   topGames(): Observable<igGame[]> {
-    return this.http.post<igGame[]>('/externalgames/games', this.topBody, {
+    this.body = searchStrings.topBody
+    return this.http.post<igGame[]>('/externalgames/games', searchStrings.topBody, {
       headers: this.headers,
     });
   }
   infiniteGames(): Observable<igGame[]> {
-    let infiniteBody =
-      'fields name, cover.url; limit 50; where rating != null & category = 0; sort total_rating desc; offset ' +
-      this.offset +
-      ';';
+    let infiniteBody = this.body + ' offset ' + this.offset + ';';
     this.offset += 50;
     console.log('offset is: ', this.offset);
     console.log(infiniteBody);
@@ -39,22 +52,22 @@ export class IgdbResultsService {
   }
 
   searchGames(term: string | null): Observable<igGame[]> {
-    let searchString =
-      'fields name, cover.url; limit 200; search ' + '"' + term + '";';
-    return this.http.post<igGame[]>('/externalgames/games', searchString, {
+    let filterString =
+      searchStrings.searchString + '"' + term + '";';
+    return this.http.post<igGame[]>('/externalgames/games', filterString, {
       headers: this.headers,
     });
   }
   searchPlatforms(platformId: number): Observable<igGame[]> {
-    let filterString =
-      'fields name, cover.url; limit 200; where category = 0 & release_dates.platform = ' + platformId + ' & aggregated_rating != null & rating != null; sort total_rating desc;'
+    let filterString = searchStrings.platformString1 + platformId + searchStrings.platformString2;
+    this.body = filterString;
     return this.http.post<igGame[]>('/externalgames/games', filterString, {
       headers: this.headers,
     });
   }
   searchGenres(genreId: number): Observable<igGame[]> {
-    let filterString =
-      'fields name, cover.url; limit 200; where category = 0 & genres = ' + genreId + ' & aggregated_rating != null & rating != null; sort total_rating desc;'
+    let filterString = searchStrings.genreString1 + genreId + searchStrings.genreString2;
+    this.body = filterString;
     return this.http.post<igGame[]>('/externalgames/games', filterString, {
       headers: this.headers,
     });
