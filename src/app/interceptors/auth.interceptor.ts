1797b1852b1e4ca8445b16f042830f0d77a6193c
Matt <Mvjarrett92@gmail.com>
2022-12-16 13:10:30 -0500

    reverse changes

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,37 +1,37 @@
-// import {
-//   HttpEvent,
-//   HttpHandler,
-//   HttpInterceptor,
-//   HttpRequest,
-// } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-// @Injectable()
-// export class AuthInterceptor implements HttpInterceptor {
-//   intercept(
-//     req: HttpRequest<any>,
-//     next: HttpHandler
-//   ): Observable<HttpEvent<any>> {
-//     const jwt = localStorage.getItem('token');
-//     const idToken = localStorage.getItem('user_id');
-//     const igdb_id = localStorage.getItem('igdb_id');
-//     const igdb_token = localStorage.getItem('igdb_token');
-//     // const addId = payload.id
-//     if (jwt && idToken && igdb_id && igdb_token) {
-//       const cloned = req.clone({
-//         headers: req.headers
-//           .set('jwt', jwt)
-//           .set('user_id', idToken)
-//           .set('Client-ID', igdb_id)
-//           .set('Authorization', igdb_token)
-//           .set('Access-Control-Allow-Origin', '*')
-//       });
-//       return next.handle(cloned);
-//     } else {
-//       return next.handle(req);
-//     }
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const jwt = localStorage.getItem('token');
+    const idToken = localStorage.getItem('user_id');
+    const igdb_id = localStorage.getItem('igdb_id');
+    const igdb_token = localStorage.getItem('igdb_token');
+    // const addId = payload.id
+    if (jwt && idToken && igdb_id && igdb_token) {
+      const cloned = req.clone({
+        headers: req.headers
+          .set('jwt', jwt)
+          .set('user_id', idToken)
+          .set('Client-ID', igdb_id)
+          .set('Authorization', igdb_token)
+          .set('Access-Control-Allow-Origin', '*')
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
 
     
-//   }
-// }
+  }
+}
