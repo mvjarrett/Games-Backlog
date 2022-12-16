3b845e71ad231b4ab593a8ffe22c9e970b33f343
Matt <Mvjarrett92@gmail.com>
2022-12-16 11:45:51 -0500

    update env, procfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // serverUrl: 'http://localhost:8080',
+  serverUrl: 'http://localhost:8080',
   headers : {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
